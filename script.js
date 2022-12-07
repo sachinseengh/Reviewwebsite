ce7d2e2423aab9b8c9da1408953fc5e610c57c16
sachinseengh <sachinseengh@gmail.com>
2022-12-07 20:18:53 +0545

    dec 7

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 //dark theme button
 
-const section =document.querySelectorAll('.sections')
-console.log(section );
-const tops=document.querySelector('.Notes');
-console.log( tops);
-let theme=document.querySelector('.theme');
-theme.addEventListener('click',function( ){
-    tops.classList.toggle('dark-theme');
-    for(let i=0;i<section.length;i++){
-    section[i].classList.toggle('dark-theme');
-};
-});
+// const section =document.querySelectorAll('.sections')
+// console.log(section );
+// const tops=document.querySelector('.Notes');
+// console.log( tops);
+// let theme=document.querySelector('.theme');
+// theme.addEventListener('click',function( ){
+//     tops.classList.toggle('dark-theme');
+//     for(let i=0;i<section.length;i++){
+//     section[i].classList.toggle('dark-theme');
+// };
+// });
