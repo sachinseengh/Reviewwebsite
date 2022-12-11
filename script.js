eaf4c49dea00632bfd0e504c5602c3f66f0f6d2a
sachinseengh <sachinseengh@gmail.com>
2022-12-11 23:20:03 +0545

    smoothscrolligsectionnotformobile

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // dark theme button
 
 const section =document.querySelectorAll('.sections')
@@ -21,3 +21,78 @@ theme.addEventListener('click',function( ){
 };
 
 });
+
+
+const nav=document.querySelector('.nav');
+
+const handleHover = function (e, opacity) {
+    if (e.target.classList.contains('nav__link')) {
+      const link = e.target;
+      const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+      const logo = link.closest('.nav').querySelector('img');
+  
+      siblings.forEach(el => {
+        if (el !== link) el.style.opacity = opacity;
+      });
+      logo.style.opacity = opacity;
+    }
+  };
+  nav.addEventListener('mouseover', function (e) {
+    handleHover(e, 0.5);
+  });
+  
+  nav.addEventListener('mouseout', function (e) {
+    handleHover(e, 1);
+  });
+
+
+
+
+  const section1 = document.querySelector('#section--1');
+const section2= document.querySelector('#section--2');
+const section3= document.querySelector('#section--3');
+const section4= document.querySelector('#section--4');
+const section5= document.querySelector('#section--5');
+
+
+//nav link items
+
+const item1=document.querySelector('.link-1');
+const item2=document.querySelector('.link-2');
+const item3=document.querySelector('.link-3');
+const item4=document.querySelector('.link-4');
+const item5=document.querySelector('.link-5');
+
+  
+
+// const btnScrollTo = document.querySelector('.nav');
+const btnScrollTo = document.querySelector('.one');
+
+// console.log( btnScrollTo);
+
+item1.addEventListener('click', function (e) {
+    e.preventDefault( );
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+item2.addEventListener('click', function (e) {
+    e.preventDefault( );
+
+  section2.scrollIntoView({ behavior: 'smooth' });
+});
+item3.addEventListener('click', function (e) {
+    e.preventDefault( );
+
+  section3.scrollIntoView({ behavior: 'smooth' });
+});
+item4.addEventListener('click', function (e) {
+    e.preventDefault( );
+
+  section4.scrollIntoView({ behavior: 'smooth' });
+});
+item5.addEventListener('click', function (e) {
+    e.preventDefault( );
+
+  section5.scrollIntoView({ behavior: 'smooth' });
+});
+
